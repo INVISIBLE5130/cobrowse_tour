529bb088afa34925a2fcc111c08ab9296653212c
INVISIBLE5130 <goshakovan14@gmail.com>
2020-10-07 17:35:56 +0300

    Code was refactored for half opacity black background of the tour

--- js/tour.js
+++ js/tour.js
@@ -102,38 +102,33 @@ function backScreen() {
             document.querySelector('.menu-icon').style.backgroundColor = 'white'
             document.querySelector('.user-option-list').children[0].style.zIndex = 'unset'
             document.querySelector('.user-option-list').children[0].style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-plus').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
             break;
         case 7:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
-            document.querySelector('.tour_window-title').innerHTML = 'Minus scope'
+            document.querySelector('.tour_window-title').innerHTML = 'Plus scope'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could change' +
-                ' scope to minus.'
+                ' scope to plus.'
             document.querySelector('.user-option-list').children[0].style.zIndex = '1001'
             document.querySelector('.user-option-list').children[0].style.backgroundColor = 'white'
             document.querySelector('.user-option-list').children[1].style.zIndex = 'unset'
             document.querySelector('.user-option-list').children[1].style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-plus').style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-minus').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+
             break;
         case 8:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
-            document.querySelector('.tour_window-title').innerHTML = 'Plus scope'
+            document.querySelector('.tour_window-title').innerHTML = 'Minus scope'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could change' +
-                ' scope to plus.'
+                ' scope to minus.'
             document.querySelector('.user-option-list').children[1].style.zIndex = '1001'
             document.querySelector('.user-option-list').children[1].style.backgroundColor = 'white'
             document.querySelector('.user-option-list').children[2].style.zIndex = 'unset'
             document.querySelector('.user-option-list').children[2].style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-minus').style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-pencil').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+
             break;
         case 9:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'Pencil'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could draw' +
                 ' anything over the screen or if you use tool on mobile device you could touch and hold your screen to activate it.'
@@ -141,12 +136,11 @@ function backScreen() {
             document.querySelector('.user-option-list').children[2].style.backgroundColor = 'white'
             document.querySelector('.user-option-list').children[3].style.zIndex = 'unset'
             document.querySelector('.user-option-list').children[3].style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-pencil').style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-hand').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+
             break;
         case 10:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'Drag'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could move' +
                 ' the screen of presentation page.'
@@ -154,14 +148,13 @@ function backScreen() {
             document.querySelector('.user-option-list').children[3].style.backgroundColor = 'white'
             document.querySelector('.user-option-list').children[4].style.zIndex = 'unset'
             document.querySelector('.user-option-list').children[4].style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-hand').style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-clip').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+
 
             document.querySelector('.next').style.display = 'unset'
             break;
         case 11:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'File'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could ' +
                 ' drag & drop your files to the presentation and show it.'
@@ -169,6 +162,7 @@ function backScreen() {
             document.querySelector('.user-option-list').children[3].style.backgroundColor = 'unset'
             document.querySelector('.user-option-list').children[4].style.zIndex = '1001'
             document.querySelector('.user-option-list').children[4].style.backgroundColor = 'white'
+
             break;
     }
 }
@@ -178,11 +172,6 @@ function nextScreen() {
 
     switch (screen) {
         case 1:
-            if (window.innerWidth < 1023) {
-                document.querySelector('.user-header').style.position = 'absolute'
-                document.querySelector('.user-header').style.bottom = '10%'
-                document.querySelector('.user-header').style.zIndex = 'unset'
-            }
             document.querySelector('.tour_window-title').innerHTML = 'Chat'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could open ' +
                 ' the chat and texting with other participants of this session.'
@@ -239,69 +228,44 @@ function nextScreen() {
             document.querySelector('.menu-icon').style.backgroundColor = 'white'
             break;
         case 7:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
-            document.querySelector('.tour_window-title').innerHTML = 'Minus scope'
-            document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could change' +
-                ' scope to minus.'
-            document.querySelector('.menu-icon').style.zIndex = 'unset'
-            document.querySelector('.menu-icon').style.backgroundColor = 'unset'
-            document.querySelector('.user-option-list').children[0].style.zIndex = '1001'
-            document.querySelector('.user-option-list').children[0].style.backgroundColor = 'white'
-            break;
-        case 8:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'Plus scope'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could change' +
                 ' scope to plus.'
-            document.querySelector('.user-option-list').children[0].style.zIndex = 'unset'
-            document.querySelector('.user-option-list').children[0].style.backgroundColor = 'unset'
-            document.querySelector('.user-option-list').children[1].style.zIndex = '1001'
-            document.querySelector('.user-option-list').children[1].style.backgroundColor = 'white'
+            document.querySelector('.menu-icon').style.zIndex = 'unset'
+            document.querySelector('.menu-icon').style.backgroundColor = 'unset'
+            document.querySelector('.tour_window-right-block-plus').style.backgroundColor = 'unset'
+
+            break;
+        case 8:
+            document.querySelector('.tour_window-title').innerHTML = 'Minus scope'
+            document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could change' +
+                ' scope to minus.'
+            document.querySelector('.tour_window-right-block-plus').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+            document.querySelector('.tour_window-right-block-minus').style.backgroundColor = 'unset'
+
             break;
         case 9:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'Pencil'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could draw' +
                 ' anything over the screen or if you use tool on mobile device you could touch and hold your screen to activate it.'
-            document.querySelector('.user-option-list').children[1].style.zIndex = 'unset'
-            document.querySelector('.user-option-list').children[1].style.backgroundColor = 'unset'
-            document.querySelector('.user-option-list').children[2].style.zIndex = '1001'
-            document.querySelector('.user-option-list').children[2].style.backgroundColor = 'white'
+            document.querySelector('.tour_window-right-block-minus').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+            document.querySelector('.tour_window-right-block-pencil').style.backgroundColor = 'unset'
+
             break;
         case 10:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'Drag'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could move' +
                 ' the screen of presentation page.'
-            document.querySelector('.user-option-list').children[2].style.zIndex = 'unset'
-            document.querySelector('.user-option-list').children[2].style.backgroundColor = 'unset'
-            document.querySelector('.user-option-list').children[3].style.zIndex = '1001'
-            document.querySelector('.user-option-list').children[3].style.backgroundColor = 'white'
+            document.querySelector('.tour_window-right-block-pencil').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+            document.querySelector('.tour_window-right-block-hand').style.backgroundColor = 'unset'
+
             break;
         case 11:
-            document.querySelector('.user-option-slide').style.position = 'absolute'
-            document.querySelector('.user-option-slide').style.bottom = 'unset'
-            document.querySelector('.user-option-slide').style.zIndex = 'unset'
-
             document.querySelector('.tour_window-title').innerHTML = 'File'
             document.querySelector('.tour_window-description').innerHTML = 'Clicking on this button you could ' +
                 ' drag & drop your files to the presentation and show it.'
-            document.querySelector('.user-option-list').children[3].style.zIndex = 'unset'
-            document.querySelector('.user-option-list').children[3].style.backgroundColor = 'unset'
-            document.querySelector('.user-option-list').children[4].style.zIndex = '1001'
-            document.querySelector('.user-option-list').children[4].style.backgroundColor = 'white'
+            document.querySelector('.tour_window-right-block-hand').style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+            document.querySelector('.tour_window-right-block-clip').style.backgroundColor = 'unset'
 
             document.querySelector('.next').style.display = 'none'
             break;
